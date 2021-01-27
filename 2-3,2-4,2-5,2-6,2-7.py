5c6f5927a96446701cab57ab38c7a59d5e7f366b
MasterLee028 <77769260+MasterLee028@users.noreply.github.com>
2021-01-27 13:15:52 +0800

    2-3,4,5,6,7作业代码
    2-3,4,5,6,7作业代码

---
+++ 2-3,2-4,2-5,2-6,2-7.py
@@ -0,0 +1,33 @@
+#2-3
+infor1="hello eric"
+infor2="would you like to learn some"
+infor3="Python today?"
+infor=infor1.title()+","+infor2+" "+infor3
+print(infor)
+#2-4
+first_name="james"
+last_name="harden"
+full_name=first_name+" "+last_name
+print(full_name.title())
+print(full_name.upper())
+print(full_name.lower())
+#2-5
+name="albert einstein"
+infor_1="once said"
+infor_2='"A person who never made a mistake never tried anything new"'
+stence=name.title()+" "+infor_1+" "+infor_2
+print(stence)
+#2-6
+famous_person="albert einstein"
+infor_1="once said"
+infor_2='"A person who never made a mistake never tried anything new"'
+message=famous_person.title()+" "+infor_1+" "+infor_2
+print(message)
+#2-7
+first_name="\tjames\n "
+last_name="harden "
+full_name=first_name+" "+last_name
+full_name.rstrip()
+full_name.lstrip()
+full_name=full_name.strip()
+print(full_name)
