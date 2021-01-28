287ac8d96c64a32cce54419d1afb39abac372c20
MasterLee028 <77769260+MasterLee028@users.noreply.github.com>
2021-01-28 12:14:40 +0800

    作业代码2-8,2-9,2-10
    作业代码2-8,2-9,2-10

---
+++ 2-8,2-9，2-10.py
@@ -0,0 +1,16 @@
+#2-8
+print(4+4)
+print(16/2)
+print(4*2)
+print(12-4)
+#2-9
+number=7
+message="My favorite number is "+str(number)
+print(message)
+#2-10
+name="albert einstein"
+infor_1="once said"
+infor_2='"A person who never made a mistake never tried anything new"'
+stence=name.title()+" "+infor_1+" "+infor_2
+print(stence)
+#这是一条注释，选择了一句名人说的话，并将他的话打印出来
