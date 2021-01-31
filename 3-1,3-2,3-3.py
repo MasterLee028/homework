d18a828da83ec513ba82276bbb494d2b499082e0
MasterLee028 <77769260+MasterLee028@users.noreply.github.com>
2021-01-31 14:55:53 +0800

    3-1,3-2,3-3
    3-1,3-2,3-3代码作业

---
+++ 3-1,3-2,3-3.py
@@ -0,0 +1,19 @@
+#3-1
+names=['james','harden','curry','kuzma','cook']
+print(names)
+#3-2
+names=['james','harden','curry','kuzma','cook']
+message1=names[0].title()+','+"how are you?"
+message2=names[1].title()+','+"how are you?"
+message3=names[2].title()+','+"how are you?"
+message4=names[3].title()+','+"how are you?"
+message5=names[4].title()+','+"how are you?"
+print(message1)
+print(message2)
+print(message3)
+print(message4)
+print(message5)
+#3-3
+vehicle=['bike','car','bus','taxi','foot']
+message="I would like to own a "+vehicle[0].title()+'.'
+print(message)
