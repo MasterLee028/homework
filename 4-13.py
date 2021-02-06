671b61b72dbd28864bd5fcde409c04caee1bcf3c
MasterLee028 <77769260+MasterLee028@users.noreply.github.com>
2021-02-06 13:24:54 +0800

    4-13
    4-13作业代码

---
+++ 4-13.py
@@ -0,0 +1,8 @@
+#4-13
+foods=('meat','sheep','fire rice','noodle','water')
+for food in foods:
+    print(food)
+#foods[1]=('rice')    已验证程序会报错
+mod_foods=('meat','pork','fire rice','vegetable','water')
+for mod_food in mod_foods:
+    print(mod_food)
