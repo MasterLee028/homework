5d27525bd639b82c81234b6c55b0bc8c30d9da49
MasterLee028 <77769260+MasterLee028@users.noreply.github.com>
2021-02-13 17:10:33 +0800

    Add files via upload
    6-1，6-2,6-3作业代码

---
+++ 6-1，6-2,6-3.py
@@ -0,0 +1,29 @@
+#6-1
+infor={'first_name':'lee','last_name':'tom','age':'20','city':'cheng du'}
+print(infor)
+#6-2
+number={'lee':'7','tom':'1','wang':'2','zhang':'3','liu':'4'}
+print("Lee's favorite number is "+number['lee']+".")
+print("tom's favorite number is "+number['tom']+".")
+print("wang's favorite number is "+number['wang']+".")
+print("zhang's favorite number is "+number['zhang']+".")
+print("liu's favorite number is "+number['liu']+".")
+#6-3
+word={'print':'打印','append':'在列表末尾添加元素','del':'删除','pop':'冒出删除','remove':'可使用元素的值的删除','range':'创建数字列表'}
+print("print:"+
+      word['print'].title()+
+      ".")
+print("append:"+
+      word['append'].title()+
+      ".")
+print("del:"+
+      word['del'].title()+
+      ".")
+print("pop:"+
+      word['pop'].title()+
+      ".")
+print("range:"+
+      word['range'].title()+
+      ".")
+
+
